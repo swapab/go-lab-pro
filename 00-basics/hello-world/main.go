ea6326fea76c9b7094480786153e7a18e2273dcb
Swapnil Abnave <swapnil.shantaram.abnave@cariad.technology>
2025-10-28 11:37:30 +0100

    update hello-world example

--- 00-basics/hello-world/main.go
+++ 00-basics/hello-world/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println("Go Version:", runtime.Version())
+	fmt.Println("OS/Arch:", runtime.GOOS, runtime.GOARCH)
+	fmt.Println("NumCPU:", runtime.NumCPU())
+	fmt.Println("Current Time:", time.Now())
 }
